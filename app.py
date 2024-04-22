fb7dcae5e2d9a511879f378a525ead44ff820b5b
caioedurib <caioedurib@gmail.com>
2024-04-22 19:44:54 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# TODO:
+#       - Get functions for each action a user can take
+#       - List the variables in
+#       - Define what is displayed
+#       - Figure out how the input is gathered (GET/POST/Form?)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/inputcompound")
+def input():
+
+    return "<b>input command</b>"
+
+@app.route("/showcompound/<int:compid>")
+def showcompound(compid):
+    compid = compid * 2
+    return render_template('compound.html', compid=compid)
