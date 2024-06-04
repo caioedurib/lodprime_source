2aa07cef849e1e96af15034883fdf39a17cb69b7
caioedurib <caioedurib@gmail.com>
2024-06-04 16:41:13 +0100

    basic test_model function done in train_model.py

--- train_model.py
+++ train_model.py
@@ -3,143 +3,9 @@ Code to train and save the classifier model. Reusable if the source dataset file
 Saved model file (pickled) is loaded by classify_inputs.py
 '''
 import pickle
-import random
-import csv
-from csv import reader
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from os import getenv
-#pd.options.mode.chained_assignment = None #this can be a future issue, but is required to remove a warning every time
-
-def train_RF_weighted(X_train, X_test, y_train, y_test, max_features):
-    if np.all(y_test == 0) or np.all(y_test == 1):
-        print('Constant test set error.')
-        return 0, 0, 0, 0, 0, 0
-    rf = RandomForestClassifier(n_estimators=500, class_weight='balanced_subsample', max_features=max_features, random_state=0)
-    rf = rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_test)
-    y_pred_prob = rf.predict_proba(X_test)
-    TN, FP, FN, TP = confusion_matrix(y_test, y_pred).ravel()
-    precision, recall, thresholds = precision_recall_curve(y_test, y_pred_prob[:, 1])
-    auc_precision_recall = auc(recall, precision)
-    AUC = roc_auc_score(y_test, y_pred_prob[:, 1])  # sample_weight parameter if there was manual weights
-    return round(TP,2), round(TN,2), round(FP,2), round(FN,2), round(AUC,3), round(auc_precision_recall, 3)
-
-
-#Returns the total weight of positive and negative examples in a dataframe (input is a dataframe filtered for a single compound)
-def get_compound_weights(df):
-    try:
-        pos_weight = df[df['class'] == 1].sum()
-    except KeyError:
-        pos_weight = 0
-    try:
-        neg_weight = df[df['class'] == 0].sum()
-    except KeyError:
-        neg_weight = 0
-    return pos_weight, neg_weight
-
-#Find which of the folds in the CV should receive a compound, based on its pos/neg weights and the current weights in the folds
-def find_best_fit(Folds_Dictionary, compound_pos_weight, compound_neg_weight, pos_class_fold_weight, neg_class_fold_weight):
-    best_fit = "Fold1"
-    min_overflow = 999
-    for fold in Folds_Dictionary:
-        fold_pos_weight = Folds_Dictionary[fold]["pos_weight"]
-        fold_neg_weight = Folds_Dictionary[fold]["neg_weight"]
-        pos_weight_overflow = (fold_pos_weight + compound_pos_weight) - pos_class_fold_weight
-        neg_weight_overflow = (fold_neg_weight + compound_neg_weight) - neg_class_fold_weight
-        if pos_weight_overflow < 0 and neg_weight_overflow < 0:
-            return fold # compound completely fits inside the current fold, set it there
-        elif pos_weight_overflow > 0 and neg_weight_overflow > 0:
-            total_compound_overflow = pos_weight_overflow + neg_weight_overflow
-            if total_compound_overflow < min_overflow:
-                best_fit = fold
-                min_overflow = total_compound_overflow
-        elif pos_weight_overflow > 0:
-            if pos_weight_overflow < min_overflow:
-                best_fit = fold
-                min_overflow = pos_weight_overflow
-        else:
-            if neg_weight_overflow < min_overflow:
-                best_fit = fold
-                min_overflow = neg_weight_overflow
-    return best_fit
-
-
-def create_Folds_Dictionary(df, random_seed, element_id_attribute):
-    compounds_list = list(df[element_id_attribute].unique())
-    random.seed(random_seed)
-    random.shuffle(compounds_list) #shuffle list of compounds so that dataset order has no bearing on result
-    n_instances = df.shape[0]
-    positive_class_count = df['class'].value_counts()[1]
-    pos_class_fold_weight = round(positive_class_count/5, 2)
-    neg_class_fold_weight = round(n_instances/5 - positive_class_count/5, 2)
-    Folds_Dictionary = {"Fold1": {"pos_weight": 0, "neg_weight": 0, "compound_list": []},
-            "Fold2": {"pos_weight": 0, "neg_weight": 0, "compound_list": []},
-            "Fold3": {"pos_weight": 0, "neg_weight": 0, "compound_list": []},
-            "Fold4": {"pos_weight": 0, "neg_weight": 0, "compound_list": []},
-            "Fold5": {"pos_weight": 0, "neg_weight": 0, "compound_list": []}}
-
-    for compound in compounds_list:
-        compound_df = df[df[element_id_attribute] == compound]
-        compound_neg_weight = compound_df[compound_df['class'] == 0].shape[0]
-        compound_pos_weight = compound_df[compound_df['class'] == 1].shape[0]
-        TargetFold = find_best_fit(Folds_Dictionary, compound_pos_weight, compound_neg_weight, pos_class_fold_weight, neg_class_fold_weight)
-        Folds_Dictionary[TargetFold]["pos_weight"] = Folds_Dictionary[TargetFold]["pos_weight"] + compound_pos_weight
-        Folds_Dictionary[TargetFold]["neg_weight"] = Folds_Dictionary[TargetFold]["neg_weight"] + compound_neg_weight
-        Folds_Dictionary[TargetFold]["compound_list"].append(compound)
-    return Folds_Dictionary
-
-
-
-def print_metrics(TP_array, FP_array, TN_array, FN_array, AUC_array, PR_AUC_array, write_obj):
-    global _cumulative_AUC
-    print('Fold\tTP\tFP\tTN\tFN\tAUC\tPR_AUC')
-    write_obj.write('Fold\tTP\tFP\tTN\tFN\tAUC\tPR_AUC\n')
-    for i in range(0, 5):
-        print(f'{i}\t{TP_array[i]}\t{FP_array[i]}\t{TN_array[i]}\t{FN_array[i]}\t{AUC_array[i]}\t{PR_AUC_array[i]}')
-        write_obj.write(f'{i}\t{TP_array[i]}\t{FP_array[i]}\t{TN_array[i]}\t{FN_array[i]}\t{AUC_array[i]}\t{PR_AUC_array[i]}\n')
-    print(f'Avg/Median\t{round(np.sum(TP_array),3)}\t{round(np.sum(FP_array),3)}\t{round(np.sum(TN_array),3)}\t{round(np.sum(FN_array),3)}\t{round(np.median(AUC_array),3)}\t{round(np.median(PR_AUC_array),3)}')
-    write_obj.write(f'Avg/Median\t{round(np.sum(TP_array),3)}\t{round(np.sum(FP_array),3)}\t{round(np.sum(TN_array),3)}\t{round(np.sum(FN_array),3)}\t{round(np.median(AUC_array),3)}\t{round(np.median(PR_AUC_array),3)}\n')
-
-
-
-
-
-
-
-
-
-
-def cross_validation(df, apply_filter, random_seed, write_obj):
-    Compound_ID_feature = 'Compound_name'
-    write_obj.write(f'Instances: {df.shape[0]} - Features: {df.shape[1]}')
-    TP_array = []
-    FP_array = []
-    TN_array = []
-    FN_array = []
-    AUC_array = []
-    PR_AUC_array = []
-    Folds_Dictionary = create_Folds_Dictionary(df, random_seed, Compound_ID_feature)
-    for fold in Folds_Dictionary:
-        training_set = df.loc[~df[Compound_ID_feature].isin(Folds_Dictionary[fold]['compound_list'])]
-        test_set = df.loc[df[Compound_ID_feature].isin(Folds_Dictionary[fold]['compound_list'])]
-        training_set = training_set.drop([Compound_ID_feature], 1) # drop compound name feature
-        test_set = test_set.drop([Compound_ID_feature], 1) # drop compound name feature
-        X_train, X_test = training_set.iloc[:, :-1], test_set.iloc[:, :-1]
-        y_train, y_test = training_set.iloc[:, -1], test_set.iloc[:, -1]
-        max_features = "auto"  # default values
-        TP, TN, FP, FN, AUC, PR_AUC = train_RF_weighted(X_train, X_test, y_train, y_test, max_features)
-        TN_array.append(TN)
-        FP_array.append(FP)
-        FN_array.append(FN)
-        TP_array.append(TP)
-        AUC_array.append(AUC)
-        PR_AUC_array.append(PR_AUC)
-    print_metrics(TP_array, FP_array, TN_array, FN_array, AUC_array, PR_AUC_array, write_obj)
-    return 1
-
-
 
 def removeLowFrequencyFeatures(df, threshold):
     if threshold < 0:
@@ -162,9 +28,6 @@ def removeLowFrequencyFeatures(df, threshold):
     return removeInvalidInstances(df)
 
 
-
-#TODO: STOPPED HERE
-
 def removeInvalidInstances(df):
     df = df.reset_index()
     df = df.drop('Index', axis=1)
@@ -174,10 +37,6 @@ def removeInvalidInstances(df):
         features_only_df = features_only_df.drop('sex', axis=1)
     except:
         print('Warning: sex feature not found when trying to remove it for removeLowFrequencyFeatures. Expected for M+F datasets.')
-    try:
-        features_only_df = features_only_df.drop('Compound_name', axis=1)
-    except:
-        print('Warning: Compound_name feature not found when trying to remove it for removeLowFrequencyFeatures.')
     drop_instances = []
     for key in range(len(features_only_df)):
         if features_only_df.iloc[key].sum() == 0:  # remove instances with only 0 values
@@ -214,22 +73,27 @@ def load_dataset(path, fixed_sex):
     return df
 
 
+# loads a dataset, trains a model using all its data and tests saving it to a file, loading the model from the file
+# and using it to make a prediction on dummy data.
 def test_model():
-    df = load_dataset('static/files/datasets/MM Molecular Fingerprints dataset.tsv', "M")  # if fixed_sex is M or F, this will filter the dataset to include only instances from that sex.
+    df = load_dataset('static/files/datasets/MM Molecular Fingerprints dataset.tsv', "all")  # if fixed_sex is M or F, this will filter the dataset to include only instances from that sex.
+    rf = RandomForestClassifier(n_estimators=500, class_weight='balanced_subsample', random_state=0)
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+    rf = rf.fit(X.values, y)  # using X.values removes a warning about feature names in X
+    with open("static/files/models/classifier.pkl", "wb") as f:
+        pickle.dump(rf, f, protocol=pickle.HIGHEST_PROTOCOL)
+    with open("static/files/models/classifier.pkl", "rb") as f:
+        rf_model = pickle.load(f)
+        list_predictions = []
+        test_entry = [np.zeros(len(X.columns))]   # this will be a list of lists, each with len(X.columns) elements
+        y_pred_prob = rf_model.predict_proba(test_entry)
+        list_pred = y_pred_prob.tolist()
+        for i in range(0, len(list_pred)):
+            list_predictions.append(list(list_pred[i])[1])
+            print(list_predictions)
 
 
 if __name__ == "__main__":
     test_model()  # False: NoFilter, False: skip grid search
     print('All done!')
-'''    
-    #test model
-    from sklearn import ensemble
-    from sklearn import datasets
-    clf = ensemble.HistGradientBoostingClassifier()
-    X, y = datasets.load_iris(return_X_y=True)
-    clf.fit(X, y)
-    
-    #test pickle savefile
-    with open("static/files/classifier.pkl", "wb") as f:
-        pickle.dump(clf, f, protocol=pickle.HIGHEST_PROTOCOL)
-'''
